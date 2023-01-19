0c48193b85e75cc6ec7ab5d8a575d29aba8d3a88
Tatsunori Uchino <tats.u@live.jp>
2023-01-19 23:45:31 +0900

    Improve mono fonts a little

--- styles/font.css.ts
+++ styles/font.css.ts
@@ -1,6 +1,6 @@
 import { style } from "@vanilla-extract/css";
 
 export const monoFontClass = style({
-  fontFamily: `SF Mono, Cascadia Code, Fira Code, Fira Mono, Firge35, Firge35 Console, Firge35Nerd Console, Firge35Nerd, Firge, Firge Console, FirgeNerd, FirgeNerd Console, HackGen35, HackGen35 Console, HackGen, HackGen Console, Menlo, Consolas, Inconsolata, Ubuntu Mono, monospace`,
+  fontFamily: `SF Mono, Fira Code, Source Code Pro, Cascadia Code, Fira Mono, Firge35, Firge35 Console, Firge35Nerd Console, Firge35Nerd, Dejavu Sans Mono, Firge, Firge Console, FirgeNerd, FirgeNerd Console, HackGen35, HackGen35 Console, HackGen, HackGen Console, Menlo, Consolas, Inconsolata, Ubuntu Mono, monospace`,
   fontVariantLigatures: "none",
 });
